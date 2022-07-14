e935d77a80e8549b2eb068f70566ccbfe3c6d486
Vojta Kasina <104126310+VojtaKasina@users.noreply.github.com>
2022-07-14 19:17:59 +0200

    add rows

--- main_app.py
+++ main_app.py
@@ -2,4 +2,12 @@ import streamlit as st
 
 st.title('Moje prvni appka')
 
-st.write('Toto je moje prvni aplikace, kterou delam')
+st.write('Toto je moje prvni aplikace, kterou delam.')
+
+page = st.sidebar.radio('Select page',['Test','Thomson'])
+
+if page = 'Test':
+    st.write('Toto je moje prvni aplikace, kterou delam.')
+
+if page == 'Thompson':
+    st.write('Thompson sampling')
